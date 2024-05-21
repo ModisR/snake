33fe90a89afb02f9c0d824322904a7c686fd0f9f
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-05-21 14:41:25 +0100

    set up assets & canvas

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+const canvas = document.getElementsByTagName('canvas')[0];
+const ctx = canvas.getContext('2d');
+
+const tileSize = 32;
+const tilesX = 24;
+const tilesY = 16;
+
+const width = tilesX * tileSize;
+const height = tilesY * tileSize;
+
+canvas.width = width;
+canvas.height = height;
+
+
+ctx.moveTo(0, 0);
+ctx.lineTo(width, height);
+
+ctx.moveTo(0, height);
+ctx.lineTo(width, 0);
+
+ctx.stroke();
